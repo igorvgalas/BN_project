98af8d53c01ab9798987e8927ad5ca4d4a05ed27
Ihor Halas <igorvgalas@gmail.com>
2022-10-18 14:15:29 +0200

    make home page, makes links for services

--- beauty_nails/urls.py
+++ beauty_nails/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'services'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:service_id>', views.detail, name='detail')
 ]
--- beauty_nails/views.py
+++ beauty_nails/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 
 def index(request):
     services = Service.objects.all()
     return render(request, 'services/index.html', {'services': services})
+
+
+def detail(request, service_id):
+    service = get_object_or_404(Service, pk=service_id)
+    return render(request, 'services/detail.html', {'service': service})
