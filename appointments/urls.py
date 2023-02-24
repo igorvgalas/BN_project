9def5b4e97f04e0688a2d022c64717fcefdf7e6e
cheef <cheef@MBP-cheef.home>
2023-02-24 21:44:32 +0100

    delete appointments model

--- appointments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.appointments_index, name='appointments_index'),
-    path('<int:appointment_id>', views.appointments_detail, name='appointments_detail')]
