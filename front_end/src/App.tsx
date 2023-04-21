fe62669baf1ec472eae9022e154c8fdec0cc81ff
Ihor Halas <igorvgalas@gmail.com>
2023-04-21 12:39:23 +0200

    make NavBar component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Grid
+      templateAreas={`"header"
+                  "main"
+                  "footer"`}
+      gridTemplateColumns={"1200px 1fr"}
+      // maxW={1400}
+      // minW={1200}
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="pink.300" area={"header"}>
+        <NavBar />
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
---
+++ front_end/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} width="200px"/>
+    </HStack>
+  );
+};
+export default NavBar;
